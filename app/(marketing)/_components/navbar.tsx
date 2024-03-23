e4d5b74c34c85a27d329feefdc8217d43bf14ad0
1maker <383679422@qq.com>
2024-03-24 02:22:16 +0800

    Establish a marketing page

---
+++ app/(marketing)/_components/navbar.tsx
@@ -0,0 +1,14 @@
+import { Logo } from "@/components/ui/logo"
+
+const Navbar = () => {
+    return (
+        <div className="fixed top-0 w-full h-14 px-4 border-b
+    shadow-sm bg-white flex items-center">
+            <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+                <Logo />
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
