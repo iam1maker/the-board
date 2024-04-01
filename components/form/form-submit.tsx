c56388baa943d65e8acbdc5e034a3e13bb0158c3
im1maker <383679422@qq.com>
2024-04-01 15:14:40 +0800

    test new computer

--- components/form/form-submit.tsx
+++ components/form/form-submit.tsx
@@ -1,34 +1,40 @@
-"use client"
+"use client";
 
-import { useFormStatus } from "react-dom"
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import React from "react"
-
+import { useFormStatus } from "react-dom";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 interface FormSubmitProps {
-    children: React.ReactNode;
-    disabled?: boolean;
-    className?: string;
-    variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | "primary";
+  children: React.ReactNode;
+  disabled?: boolean;
+  className?: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | "primary";
 }
 
 export const FormSubmit = ({
-    children,
-    disabled,
-    className,
-    variant
+  children,
+  disabled,
+  className,
+  variant,
 }: FormSubmitProps) => {
-    const { pending } = useFormStatus();
-    return (
-        <Button
-            disabled={pending || disabled}
-            type="submit"
-            variant={variant}
-            size="sm"
-            className={cn(className)}
-        >
-            {children}
-        </Button>
-    )
-}
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      disabled={pending || disabled}
+      type="submit"
+      variant={variant}
+      size="sm"
+      className={cn(className)}
+    >
+      {children}
+    </Button>
+  );
+};
