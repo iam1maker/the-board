e4f1e86872c24f1a9a246f782c23b56fd406daaa
1maker <383679422@qq.com>
2024-04-03 23:28:45 +0800

    Test Board Server Action and Unsplash API

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -16,6 +16,7 @@ import { FormSubmit } from "./form-submit";
 import { Button } from "@/components/ui/button";
 import { X } from "lucide-react";
 import { toast } from "sonner";
+import { FormPicker } from "./form-picker";
 
 
 interface FormPopoverProps {
@@ -73,6 +74,10 @@ export const FormPopover = ({
                 </PopoverClose>
                 <form action={onSubmit} className=" space-y-4">
                     <div className="space-y-4">
+                        <FormPicker
+                            id="image"
+                            errors={fieldErrors}
+                        />
                         <FormInput
                             id="title"
                             label="Board title"
