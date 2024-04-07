2691bf30917f0090c09479f2da2cc13ce8d203d7
1maker <383679422@qq.com>
2024-04-07 17:50:19 +0800

    Write the board details page

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -0,0 +1,18 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { Board } from "@prisma/client";
+
+interface BoardNavBarProps {
+    data: Board;
+};
+
+export const BoardNavBar = async ({ data
+}: BoardNavBarProps) => {
+    return (
+        <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex
+        items-center px-6 gap-x-4 text-white
+        ">
+            Board Navbar!
+        </div>
+    )
+}
--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,15 @@
+import { startCase } from "lodash"
+import { auth } from "@clerk/nextjs"
+
 import { OrgControl } from "./_components/orgcontrol"
+
+export async function generateMetadata() {
+    const { orgSlug } = auth()
+    return {
+        title: startCase(orgSlug || "organization"),
+    };
+};
+
 
 const OrganizationIdLayout = ({ children }: {
     children: React.ReactNode
