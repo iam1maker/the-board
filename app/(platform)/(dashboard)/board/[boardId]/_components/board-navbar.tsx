c792bf24beea7863947092c75453f01775038610
1maker <383679422@qq.com>
2024-04-08 21:22:14 +0800

    Create board navbar and board-title-form

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { Board } from "@prisma/client";
+import { BoardTitleForm } from "./board-title-form";
 
 interface BoardNavBarProps {
     data: Board;
@@ -12,7 +13,7 @@ export const BoardNavBar = async ({ data
         <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex
         items-center px-6 gap-x-4 text-white
         ">
-            Board Navbar!
+            <BoardTitleForm data={data} />
         </div>
     )
 }
