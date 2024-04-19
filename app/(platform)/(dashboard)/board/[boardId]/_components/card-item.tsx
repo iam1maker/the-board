8a7965ef51bc09056b8ecb40d60ebf2dc57f32d7
1maker <383679422@qq.com>
2024-04-19 14:59:05 +0800

    Create Card Modal

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -2,12 +2,15 @@
 
 import { Card } from "@prisma/client";
 import { Draggable } from "@hello-pangea/dnd";
+import { useCardModal } from "@/hooks/use-card-modal";
 interface CardItemProps {
     data: Card;
     index: number;
 }
 
 export const CardItem = ({ data, index }: CardItemProps) => {
+    const cardModal = useCardModal();
+
     return (
         <Draggable draggableId={data.id} index={index}>
             {(provided) => (
@@ -16,6 +19,7 @@ export const CardItem = ({ data, index }: CardItemProps) => {
                     {...provided.dragHandleProps}
                     ref={provided.innerRef}
                     role="button"
+                    onClick={() => cardModal.onOpen(data.id)}
                     className=" truncate border-2 border-transparent hover:border-black py-2 
         px-3 text-sm bg-white rounded-md shadow-sm"
                 >
---
+++ app/api/cards/[cardId]/route.ts
@@ -0,0 +1,47 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+/**
+ * 使用 GET 请求获取特定的卡片信息。
+ * 
+ * @param req - 请求对象，包含请求的细节。
+ * @param {params} - 包含卡片ID的对象。
+ * @returns 返回一个NextResponse对象，其中包含卡片信息或者错误信息。
+ */
+export async function GET(
+    req: Request,
+    { params }: { params: { cardId: string } }
+) {
+    try {
+        // 验证用户认证信息，获取用户ID和组织ID
+        const { userId, orgId } = auth()
+        // 如果没有用户ID或组织ID，则返回未授权响应
+        if (!userId || !orgId) {
+            return new NextResponse("Unauthorized", { status: 401 })
+        }
+        // 从数据库中查找独特的卡片信息，包括其所属列表的标题
+        const card = await db.card.findUnique({
+            where: {
+                id: params.cardId,
+                list: {
+                    board: {
+                        orgId
+                    }
+                }
+            },
+            include: {
+                list: {
+                    select: {
+                        title: true
+                    }
+                }
+            }
+        })
+        // 返回卡片信息的JSON响应
+        return NextResponse.json(card)
+    } catch (error) {
+        // 如果有错误发生，则返回内部错误响应
+        return new NextResponse("Internal Error", { status: 500 })
+    }
+}
